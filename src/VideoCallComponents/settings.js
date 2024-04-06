dc2f88129173ff47801af2ff463f16a83b335420
Sindhee <btbtc21313_asanjhi@banasthali.in>
2024-04-07 04:10:35 +0530

    commit3

--- src/VideoCallComponents/settings.js
+++ src/VideoCallComponents/settings.js
@@ -1,6 +1,6 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 const appId = "b7a73b41dd2c4d5da3ef66d3a817d66b"
-const token = "007eJxTYNjEdc1JfHFAwyzpJKtVTl2bN/anHmHf5vXFcP1xtu2vf7cqMCSZJ5obJ5kYpqQYJZukmKYkGqemmZmlGCdaGJqnmJklPbjAndYQyMig99aAkZEBAkF8ToayZIXE3Mz0/GIGBgAfESIx"
+const token = "007eJxTYPidd/zkQ541WWd4rIwTayQUluv+LTP2OcR+s/FQ0ifu+74KDEnmiebGSSaGKSlGySYppimJxqlpZmYpxokWhuYpZmZJ184IpjUEMjKse3aRmZEBAkF8ToayZIXE3Mz0/GIGBgAxPSLG"
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,28 @@
-import { DownloadIcon } from '@heroicons/react/outline';
+//hero.js
 import React from 'react'
 
 function Hero() {
   return (
-    <div className="landing1 pb-8 md:pb-0">
+    <div className="landing1 pb-8 md:pb-0" style={{ backgroundColor: '#C2B280' }}>
       <div className=" landing2  md:h-[83vh] md:flex relative">
         <div className=" landing3  md:max-w-md lg:max-w-none    lg:justify-center ">
-          <h1 className=" land_h1 ">Your place to talk</h1>
-          <h2 className=" land_h2  lg:max-w-3xl w-full">
+          <h1 className=" land_h1 " >An innovative and independent platform bridging the gap between communication and information
+sharing.</h1>
+          {/* <h2 className=" land_h2  lg:max-w-3xl w-full">
             Whether you’re part of a school club, gaming group, worldwide art
             community, or just a handful of friends that want to spend time
             together, Discord makes it easy to talk every day and hang out more
             often.
-          </h2>
+          </h2> */}
           <div className="land_part1 flex flex-col sm:flex-row md:flex-col lg:flex-row md:items-start sm:items-center gap-6">
-             <button className="land_button1 items-center justify-center rounded-full p-4 text-lg hover:shadow-2xl hover:text-discord_blurple focus:outline-none transition duration-200 ease-in-out">
+             {/* <button className="land_button1 items-center justify-center rounded-full p-4 text-lg hover:shadow-2xl hover:text-discord_blurple focus:outline-none transition duration-200 ease-in-out">
              <DownloadIcon style={{ width: '1.5rem', marginRight: '0.5rem' }} />
                  Download for Mac
-            </button>
-            <button className="land_button1 rounded-full p-4 text-lg hover:shadow-2xl hover:bg-gray-800 focus:outline-none transition duration-200 ease-in-out">
+            </button> */}
+            {/* <button className="land_button1 rounded-full p-4 text-lg hover:shadow-2xl hover:bg-gray-800 focus:outline-none transition duration-200 ease-in-out">
              Open Discord in your browser
-            </button>
-        </div>
+            </button> */}
+        </div> 
         </div>
         <div className="flex-grow">
           <img
